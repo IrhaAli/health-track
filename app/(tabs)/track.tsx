7c4dcff19303a50dd52dc565a3c2000c236c9cc3
Irha Ali <irhaali_97@hotmail.ca>
2024-09-06 14:30:12 -0400

    Added calendar to track

--- app/(tabs)/track.tsx
+++ app/(tabs)/track.tsx
@@ -1,21 +1,90 @@
+import React, { useRef, useCallback } from "react";
 import { StyleSheet } from "react-native";
-import { Calendar } from "react-native-calendars";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import {
+  ExpandableCalendar,
+  AgendaList,
+  CalendarProvider,
+  WeekCalendar,
+} from "react-native-calendars";
+import testIDs from "../testIDs";
+import { agendaItems, getMarkedDates } from "../agendaItems";
+import AgendaItem from "../AgendaItem";
+import { getTheme, themeColor, lightThemeColor } from "../theme";
+
+const leftArrowIcon = require("../img/previous.png");
+const rightArrowIcon = require("../img/next.png");
+const ITEMS: any[] = agendaItems;
 
 export default function TabTwoScreen() {
+  const marked = useRef(getMarkedDates());
+  const theme = useRef(getTheme());
+  const todayBtnTheme = useRef({
+    todayButtonTextColor: themeColor,
+  });
+
+  // const onDateChanged = useCallback((date, updateSource) => {
+  //   console.log('ExpandableCalendarScreen onDateChanged: ', date, updateSource);
+  // }, []);
+
+  // const onMonthChange = useCallback(({dateString}) => {
+  //   console.log('ExpandableCalendarScreen onMonthChange: ', dateString);
+  // }, []);
+
+  const renderItem = useCallback(({ item }: any) => {
+    return <AgendaItem item={item} />;
+  }, []);
+
   return (
-    <>
-      <ThemedView>
-        <ThemedText type="title" style={styles.stepContainer}>Fast</ThemedText>
-      </ThemedView>
-    </>
+    <CalendarProvider
+      date={ITEMS[1]?.title}
+      // onDateChanged={onDateChanged}
+      // onMonthChange={onMonthChange}
+      showTodayButton
+      // disabledOpacity={0.6}
+      theme={todayBtnTheme.current}
+      // todayBottomMargin={16}
+    >
+      <ExpandableCalendar
+        testID={testIDs.expandableCalendar.CONTAINER}
+        // horizontal={false}
+        // hideArrows
+        // disablePan
+        // hideKnob
+        // initialPosition={ExpandableCalendar.positions.OPEN}
+        // calendarStyle={styles.calendar}
+        // headerStyle={styles.header} // for horizontal only
+        // disableWeekScroll
+        theme={theme.current}
+        // disableAllTouchEventsForDisabledDays
+        firstDay={1}
+        markedDates={marked.current}
+        leftArrowImageSource={leftArrowIcon}
+        rightArrowImageSource={rightArrowIcon}
+        // animateScroll
+        // closeOnDayPress={false}
+      />
+      <AgendaList
+        sections={ITEMS}
+        renderItem={renderItem}
+        // scrollToNextEvent
+        sectionStyle={styles.section}
+        // dayFormat={'yyyy-MM-d'}
+      />
+    </CalendarProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  stepContainer: {
-    marginLeft: 30,
-    marginTop: 100,
+  calendar: {
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  header: {
+    backgroundColor: "lightgrey",
+  },
+  section: {
+    backgroundColor: lightThemeColor,
+    color: "grey",
+    textTransform: "capitalize",
   },
 });
