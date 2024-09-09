ff308b2da6d971023614cf47d58ee55a2614d77e
Irha Ali <irhaali_97@hotmail.ca>
2024-09-08 22:46:49 -0400

    Add signup route

---
+++ app/(signup)/index.tsx
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+import {
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { Link } from "expo-router";
+import { SocialIcon } from "@rneui/base";
+
+export default function SignUpForm() {
+  const [username, setUsername] = useState("");
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Health App</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.input}
+          placeholder="EMAIL"
+          value={username}
+          onChangeText={setUsername}
+          autoCorrect={false}
+          autoCapitalize="none"
+        />
+      </View>
+      <View style={styles.buttonView}>
+        <Pressable style={styles.button}>
+          <Link href="/(authentication)">
+            <Text style={styles.buttonText}>SIGN UP</Text>
+          </Link>
+        </Pressable>
+      </View>
+      {/* <Text style={styles.optionsText}>OR Sign Up WITH</Text>
+      <View style={styles.socialIcons}>
+        <SocialIcon button type="facebook" />
+        <SocialIcon button type="google" />
+      </View> */}
+
+      <Text style={styles.footerText}>
+        Already Have Account?
+        <Link href="/(login)" style={styles.signup}>
+          {" "}
+          Log In
+        </Link>
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  socialIcons: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  container: {
+    alignItems: "center",
+    paddingTop: 70,
+  },
+  image: {
+    height: 160,
+    width: 170,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    textAlign: "center",
+    paddingVertical: 40,
+    color: "red",
+  },
+  inputView: {
+    gap: 15,
+    width: "100%",
+    paddingHorizontal: 40,
+    marginBottom: 5,
+  },
+  input: {
+    height: 50,
+    paddingHorizontal: 20,
+    borderColor: "red",
+    borderWidth: 1,
+    borderRadius: 7,
+  },
+  rememberView: {
+    width: "100%",
+    paddingHorizontal: 50,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    marginBottom: 8,
+  },
+  switch: {
+    flexDirection: "row",
+    gap: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  rememberText: {
+    fontSize: 13,
+  },
+  forgetText: {
+    fontSize: 11,
+    color: "red",
+  },
+  button: {
+    backgroundColor: "red",
+    height: 45,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  buttonView: {
+    width: "100%",
+    paddingHorizontal: 50,
+  },
+  optionsText: {
+    textAlign: "center",
+    paddingVertical: 10,
+    color: "gray",
+    fontSize: 13,
+    marginBottom: 6,
+  },
+  mediaIcons: {
+    flexDirection: "row",
+    gap: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 23,
+  },
+  icons: {
+    width: 40,
+    height: 40,
+  },
+  footerText: {
+    textAlign: "center",
+    color: "gray",
+  },
+  signup: {
+    color: "red",
+    fontSize: 13,
+  },
+});
