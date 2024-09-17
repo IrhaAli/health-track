f3c60d3f458ec8ba39624ea5037637f58d10dc91
Irha Ali <irhaali_97@hotmail.ca>
2024-09-17 14:37:59 -0400

    Remove authetication route

--- app/(authenticate)/[id].tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-import { useLocalSearchParams, Stack } from "expo-router";
-import { ThemeProvider } from "@react-navigation/native";
-
-export default function Id() {
-  const { id } = useLocalSearchParams();
-  return <Text>{id}</Text>;
-}
