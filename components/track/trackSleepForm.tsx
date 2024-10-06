3a15d2632fd3c14ae98742336a748395f1761201
Habib786 <habib-m-farooq@hotmail.com>
2024-10-07 01:06:50 +0300

    Created track sleep and track water components.

---
+++ components/track/trackSleepForm.tsx
@@ -0,0 +1,156 @@
+import React, { useEffect, useState } from "react";
+import DateTimePicker, { DateTimePickerEvent } from "@react-native-community/datetimepicker";
+import { StyleSheet, Text, Platform, Pressable, View } from "react-native";
+import Slider from "@react-native-community/slider";
+
+interface TrackSleepFormProps {
+    currentDate: string;
+}
+
+export default function TrackSleepForm({ currentDate }: TrackSleepFormProps) {
+    const [sleepDateTime, setSleepDateTime] = useState(new Date());
+    const [wakeupTime, setWakeupTime] = useState(new Date());
+    const [sleepQuality, setSleepQuality] = useState(0);
+    const [sleepDuration, setSleepDuration] = useState(0);
+    const [showSleepDateSelector, setShowSleepDateSelector] = useState(false);
+    const [showSleepTimeSelector, setShowSleepTimeSelector] = useState(false);
+    const [showWakeupTimeSelector, setShowWakeupTimeSelector] = useState(false);
+
+    const convertMinutesToHoursAndMinutes = (totalMinutes: number): string => 
+        `${Math.floor(totalMinutes / 60)} hours and ${totalMinutes % 60} minutes`;
+
+    const calculateSleepDuration = () => {
+        const timezoneOffsetMs = sleepDateTime.getTimezoneOffset() * 60 * 1000;
+        let sleepTimeUTC = sleepDateTime;
+        let wakeupTimeUTC = wakeupTime;
+
+        if (timezoneOffsetMs > 0) {
+            sleepTimeUTC = new Date(sleepDateTime.getTime() - timezoneOffsetMs);
+            wakeupTimeUTC = new Date(wakeupTime.getTime() - timezoneOffsetMs);
+        } else if (timezoneOffsetMs < 0) {
+            sleepTimeUTC = new Date(sleepDateTime.getTime() + timezoneOffsetMs);
+            wakeupTimeUTC = new Date(wakeupTime.getTime() + timezoneOffsetMs);
+        }
+
+        const currentHours = sleepTimeUTC.getUTCHours();
+        const currentMinutes = sleepTimeUTC.getUTCMinutes();
+        const currentSeconds = sleepTimeUTC.getUTCSeconds();
+
+        const sleepDatePickTime = new Date(
+            Date.UTC(
+                sleepDateTime.getUTCFullYear(),
+                sleepDateTime.getUTCMonth(),
+                sleepDateTime.getUTCDate(),
+                currentHours,
+                currentMinutes,
+                currentSeconds
+            )
+        );
+
+        const sleepTimeMs = sleepDatePickTime.getTime();
+        const wakeupTimeMs = wakeupTimeUTC.getTime();
+        const differenceMs = wakeupTimeMs - sleepTimeMs;
+        const differenceMinutes = differenceMs / (1000 * 60);
+        return Math.ceil(differenceMinutes);
+    };
+
+    useEffect(() => {
+        setSleepDuration(calculateSleepDuration());
+    }, [wakeupTime, sleepDateTime]);
+
+    const onSleepDateChange = (event: DateTimePickerEvent, date?: Date): void => {
+        if (event.type === "dismissed" || event.type === "set") { setShowSleepDateSelector(false); }
+        if (date) {
+            setSleepDateTime(prev => {
+                const newDateTime = new Date(prev || date);
+                newDateTime.setFullYear(date.getFullYear(), date.getMonth(), date.getDate());
+                return newDateTime;
+            });
+        }
+    }
+
+    const onSleepTimeChange = (event: DateTimePickerEvent, date?: Date): void => {
+        if (event.type === "dismissed" || event.type === "set") { setShowSleepTimeSelector(false); }
+        if (date) {
+            setSleepDateTime(prev => {
+                const newDateTime = new Date(prev || date);
+                newDateTime.setHours(date.getHours(), date.getMinutes());
+                return newDateTime;
+            });
+        }
+    }
+
+    const onWakeupTimeChange = (event: DateTimePickerEvent, date?: Date): void => {
+        if (event.type === "dismissed" || event.type === "set") { setShowWakeupTimeSelector(false); }
+        if (date) { setWakeupTime(date); }
+    }
+
+    return (
+        <View style={styles.trackSleepForm}>
+            <View>
+                <Text style={styles.lastNightSleepHeading}>Last Night's Sleep</Text>
+                {Platform.OS == "android" ?
+                    <View style={styles.sleepTimeDateView}>
+                        <Pressable onPress={() => { setShowSleepDateSelector(true); }}>
+                            <Text style={styles.sleepDateText}>{` ${sleepDateTime.toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric", })}`}</Text>
+                        </Pressable>
+                        {showSleepDateSelector && <DateTimePicker mode="date" value={sleepDateTime} onChange={onSleepDateChange} />}
+                        <Pressable onPress={() => { setShowSleepTimeSelector(true); }} >
+                            <Text style={styles.sleepTimeText}> {` ${sleepDateTime.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit", hour12: true })}`}</Text>
+                        </Pressable>
+                        {showSleepTimeSelector && <DateTimePicker mode="time" value={sleepDateTime} onChange={onSleepTimeChange} />}
+                    </View> :
+                    <View>
+                        <DateTimePicker mode="date" value={sleepDateTime} onChange={onSleepDateChange} />
+                        <DateTimePicker mode="time" value={sleepDateTime} onChange={onSleepTimeChange} />
+                    </View>}
+
+            </View>
+
+            <View>
+                <Text>Wakeup Time</Text>
+                {Platform.OS == "android" ?
+                    <View>
+                        <Pressable onPress={() => { setShowWakeupTimeSelector(true); }} >
+                            <Text style={styles.wakeupTimeText}> {` ${wakeupTime.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit", hour12: true })}`}</Text>
+                        </Pressable>
+                        {showWakeupTimeSelector && <DateTimePicker mode="time" value={wakeupTime} onChange={onWakeupTimeChange} />}
+                    </View> :
+                    <View>
+                        <DateTimePicker mode="time" value={wakeupTime} onChange={onWakeupTimeChange} />
+                    </View>
+                }
+            </View>
+
+            <View>
+                <Text>Sleep Quality: {sleepQuality}</Text>
+                <Slider value={sleepQuality} minimumValue={1} maximumValue={5} step={1} onValueChange={(value: number) => setSleepQuality(value)} />
+            </View>
+            <Text>{`Total Sleeping Hours: ${convertMinutesToHoursAndMinutes(sleepDuration)}`}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    trackSleepForm:  {
+        backgroundColor: 'white'
+    },
+    lastNightSleepHeading: {
+        fontWeight: '700',
+    },
+    sleepTimeDateView: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10
+    },
+    sleepDateText: {
+        color: 'blue'
+    },
+    sleepTimeText: {
+        color: 'blue'
+    },
+    wakeupTimeText: {
+        marginBottom: 10,
+        color: 'blue'
+    }
+})
---
+++ components/track/trackWaterForm.tsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import Dialog from "react-native-dialog";
+import { Dropdown } from "react-native-element-dropdown";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+interface TrackSleepFormProps {
+    currentDate: string;
+}
+
+enum WaterTypeEnum {
+    MILLILITRES = "millilitres",
+    LITRES = "litres",
+    GALLONS = "gallons"
+};
+
+export default function TrackWaterForm({ currentDate }: TrackSleepFormProps) {
+    const [water, setWater] = useState("");
+    const [isWaterTypeFocus, setIsWaterTypeFocus] = useState(false);
+    const [waterType, setWaterType] = useState(WaterTypeEnum.MILLILITRES);
+    
+    const waterTypeOptions = Object.values(WaterTypeEnum).map((type) => ({ label: type, value: type }));
+
+    return (
+        <View style={styles.trackWaterForm}>
+           <Dialog.Input
+                style={[ styles.input ]}
+                placeholder="Add water here..."
+                value={water}
+                onChangeText={setWater}
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="numeric"
+                maxLength={4}
+            ></Dialog.Input>
+           <Dropdown style={[ styles.dropdown, isWaterTypeFocus && { borderColor: "blue" }, styles.flexItem ]}
+                placeholderStyle={styles.placeholderStyle}
+                selectedTextStyle={styles.selectedTextStyle}
+                iconStyle={styles.iconStyle}
+                data={waterTypeOptions}
+                maxHeight={300}
+                labelField="label"
+                valueField="value"
+                placeholder={ !isWaterTypeFocus ? "Select Water Unit" : "..." }
+                value={waterType}
+                onFocus={() => setIsWaterTypeFocus(true)}
+                onBlur={() => setIsWaterTypeFocus(false)}
+                onChange={(item: any) => {
+                    setWaterType(item.value as WaterTypeEnum);
+                    setIsWaterTypeFocus(false);
+                }}
+                renderLeftIcon={() => (
+                <AntDesign style={styles.icon} color={isWaterTypeFocus ? "blue" : "black"} name="Safety" size={20} />
+                )}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    trackWaterForm:  {
+        backgroundColor: 'white',
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        paddingTop: 30, 
+        paddingBottom: 30
+    },
+    input: {
+        height: 50,
+        paddingHorizontal: 20,
+        borderColor: "gray",
+        borderWidth: 0.5,
+        borderRadius: 3,
+        fontSize: 16,
+        marginTop: 20,
+        color: 'black'
+    },
+    icon: {
+        marginRight: 5,
+    },
+    dropdown: {
+        height: 50,
+        borderColor: "gray",
+        borderWidth: 0.5,
+        borderRadius: 7,
+        paddingHorizontal: 8,
+    },
+    placeholderStyle: {
+        fontSize: 16,
+    },
+    selectedTextStyle: {
+        fontSize: 16,
+    },
+    iconStyle: {
+        width: 20,
+        height: 20,
+    },
+    flexItem: {
+        flex: 1
+    }
+})
