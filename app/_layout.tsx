b4e71002be29befb57109ec64486e5c0e247802a
Habib786 <habib-m-farooq@hotmail.com>
2024-10-09 00:21:15 +0300

    Added react native paper.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,25 @@
 import { Stack } from "expo-router";
 import { Provider } from "react-redux";
 import store from "../store/store";
+import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  },
+};
 
 export default function Layout() {
   return (
     <Provider store={store}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-      </Stack>
+      <PaperProvider theme={theme}>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
+      </PaperProvider>
     </Provider>
   );
 }
