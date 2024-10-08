26bf041551d5ff13118e467ed273324601ead642
Habib786 <habib-m-farooq@hotmail.com>
2024-10-08 19:39:43 +0300

    Set the initial value for the current date.

--- store/trackSlice.tsx
+++ store/trackSlice.tsx
@@ -5,7 +5,7 @@ interface TrackState {
 }
 
 const initialState: TrackState = {
-    currentDate: ''
+    currentDate: `${new Date().getFullYear()}-${String(new Date().getMonth() + 1).padStart(2, "0")}-${String(new Date().getDate()).padStart(2, "0")}`
 };
 
 const trackSlice = createSlice({
