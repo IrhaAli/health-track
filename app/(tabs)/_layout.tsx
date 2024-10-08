a192b1e93182d5f82ba4d028c3806f2dda17d3f8
Habib786 <habib-m-farooq@hotmail.com>
2024-10-08 19:22:58 +0300

    Format document.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,64 +9,40 @@ import { getAuth } from "firebase/auth";
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   const userId = getAuth().currentUser?.uid;
-
+  
   if (!userId) { router.push("/(signup)"); }
 
   return (
     <>
       <Stack.Screen options={{ headerShown: false }} />
-      <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-          headerShown: false,
-        }}
-      >
+
+      <Tabs screenOptions={{ tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint, headerShown: false }} >
         <Tabs.Screen
           name="index"
           options={{
             title: "Home",
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon
-                name={focused ? "home" : "home-outline"}
-                color={color}
-              />
-            ),
+            tabBarIcon: ({ color, focused }) => (<TabBarIcon name={focused ? "home" : "home-outline"} color={color} />)
           }}
         />
         <Tabs.Screen
           name="track"
           options={{
             title: "Track",
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon
-                name={focused ? "time" : "time-outline"}
-                color={color}
-              />
-            ),
+            tabBarIcon: ({ color, focused }) => (<TabBarIcon name={focused ? "time" : "time-outline"} color={color} />)
           }}
         />
         <Tabs.Screen
           name="media"
           options={{
             title: "Media",
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon
-                name={focused ? "bed" : "bed-outline"}
-                color={color}
-              />
-            ),
+            tabBarIcon: ({ color, focused }) => (<TabBarIcon name={focused ? "bed" : "bed-outline"} color={color} />)
           }}
         />
         <Tabs.Screen
           name="profile"
           options={{
             title: "Profile",
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon
-                name={focused ? "fast-food" : "fast-food-outline"}
-                color={color}
-              />
-            ),
+            tabBarIcon: ({ color, focused }) => (<TabBarIcon name={focused ? "fast-food" : "fast-food-outline"} color={color} />)
           }}
         />
       </Tabs>
