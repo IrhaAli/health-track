1284222378d1870c4940d0f46a02617a52436cde
Habib786 <habib-m-farooq@hotmail.com>
2024-10-09 21:40:41 +0300

    Added loading and currentMonth variable in track store.

--- store/trackSlice.tsx
+++ store/trackSlice.tsx
@@ -2,10 +2,14 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface TrackState {
     currentDate: string;
+    loadingTrackDate: boolean;
+    currentMonth: { month: string; year: string; }
 }
 
 const initialState: TrackState = {
-    currentDate: `${new Date().getFullYear()}-${String(new Date().getMonth() + 1).padStart(2, "0")}-${String(new Date().getDate()).padStart(2, "0")}`
+    currentDate: `${new Date().getFullYear()}-${String(new Date().getMonth() + 1).padStart(2, "0")}-${String(new Date().getDate()).padStart(2, "0")}`,
+    loadingTrackDate: true,
+    currentMonth: { month: String(new Date().getMonth() + 1).padStart(2, "0"), year: String(new Date().getFullYear()) }
 };
 
 const trackSlice = createSlice({
@@ -14,9 +18,18 @@ const trackSlice = createSlice({
     reducers: {
         setCurrentDate: (state, action: PayloadAction<string>) => {
             state.currentDate = action.payload;
-        }
+        },
+        setShowLoadingTrackDate: (state, action: PayloadAction<string>) => {
+            state.loadingTrackDate = true;
+        },
+        setHideLoadingTrackDate: (state, action: PayloadAction<string>) => {
+            state.loadingTrackDate = false;
+        },
+        setCurrentMonth: (state, action: PayloadAction<{month: string; year: string}>) => {
+            state.currentMonth = action.payload;
+        } 
     },
 });
 
-export const { setCurrentDate } = trackSlice.actions;
+export const { setCurrentDate, setShowLoadingTrackDate, setHideLoadingTrackDate, setCurrentMonth } = trackSlice.actions;
 export default trackSlice.reducer;
