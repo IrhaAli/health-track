1c4eecadfef74b6b0291f4cc997f0228fad1bac9
Habib786 <habib-m-farooq@hotmail.com>
2024-10-11 14:19:58 +0300

    Remove unused code.

--- app/(login)/index.tsx
+++ app/(login)/index.tsx
@@ -67,7 +67,7 @@ const LoginForm = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Health App</Text>
+      <Text variant="headlineLarge" style={[{ paddingVertical: 30, color: 'tomato' }]}>Health App</Text>
 
       <View style={styles.inputView}>
         <TextInput label="Email" value={email} onChangeText={setEmail} autoCorrect={false} placeholder="name@email.com" editable={!loading} autoCapitalize="none" />
@@ -91,105 +91,17 @@ const LoginForm = () => {
 };
 
 const styles = StyleSheet.create({
-  socialIcons: {
-    display: "flex",
-    flexDirection: "row",
-  },
   container: {
-    alignItems: "center",
-    paddingTop: 70,
-  },
-  image: {
-    height: 160,
-    width: 170,
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: "bold",
-    textTransform: "uppercase",
-    textAlign: "center",
-    paddingVertical: 40,
-    color: "tomato",
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   inputView: {
     gap: 15,
     width: "100%",
     paddingHorizontal: 40,
     marginBottom: 5,
-  },
-  input: {
-    height: 50,
-    paddingHorizontal: 20,
-    borderColor: "red",
-    borderWidth: 1,
-    borderRadius: 7,
-  },
-  rememberView: {
-    width: "100%",
-    paddingHorizontal: 50,
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexDirection: "row",
-    marginBottom: 8,
-  },
-  switch: {
-    flexDirection: "row",
-    gap: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  rememberText: {
-    fontSize: 13,
-  },
-  forgetText: {
-    fontSize: 11,
-    color: "red",
-  },
-  button: {
-    backgroundColor: "red",
-    height: 45,
-    borderColor: "gray",
-    borderWidth: 1,
-    borderRadius: 5,
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: 'row'
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 18,
-    fontWeight: "bold",
-    marginRight: 10
-  },
-  buttonView: {
-    width: "100%",
-    paddingHorizontal: 50,
-  },
-  optionsText: {
-    textAlign: "center",
-    paddingVertical: 10,
-    color: "gray",
-    fontSize: 13,
-    marginBottom: 6,
-  },
-  mediaIcons: {
-    flexDirection: "row",
-    gap: 15,
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 23,
-  },
-  icons: {
-    width: 40,
-    height: 40,
-  },
-  footerText: {
-    textAlign: "center",
-    color: "gray",
-  },
-  signup: {
-    color: "red",
-    fontSize: 13,
   },
   passwordEyeIcon: {
     position: 'absolute',
