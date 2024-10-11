b2c631b4051b4ae932146232399eb1583c713e04
Habib786 <habib-m-farooq@hotmail.com>
2024-10-12 00:14:37 +0300

    Add water data in store and trackWaterCard.

--- components/track/trackWaterCard.tsx
+++ components/track/trackWaterCard.tsx
@@ -1,10 +1,93 @@
-import React from "react";
-import { getAuth } from "firebase/auth";
+import React, { useEffect, useState } from "react";
 import { Card, Button, Text, Avatar } from 'react-native-paper';
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "@/firebaseConfig";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "@/store/store";
+import { pushWaterData } from "@/store/trackSlice";
+import { getAuth } from "firebase/auth";
+import { useSession } from "@/ctx";
+
+interface WaterDataEntry {
+    date: string;
+    intake_amount: number;
+    user_id: string;
+    waterType: string;
+}
+
+type WaterDataState = {
+    [key: string]: WaterDataEntry[];
+};
 
 export default function TrackWaterCard() {
-    const userId = getAuth().currentUser?.uid || "PHCJD511ukbTHQfVXPu26N8rzqg1";
+    const userId = useSelector((state: RootState) => state.user.userId);
+    const currentMonth = useSelector((state: RootState) => state.track.currentMonth);
     const LeftContent = (props: any) => <Avatar.Icon {...props} icon="glass-pint-outline" />
+    const dispatch = useDispatch();
+    const allWaterData: WaterDataState | [] = useSelector((state: RootState) => state.track.waterData);
+    const [waterData, setWaterData] = useState(null);
+    const auth = getAuth();
+
+    const { session } = useSession();
+    const user = auth.currentUser;
+    const formattedMonth: string = `${currentMonth.year}-${currentMonth.month}`; 
+
+    console.log('user?.uid', user?.uid);
+
+    if (user?.uid && currentMonth.month && currentMonth.year) {
+        const loadData = async () => {
+            try {
+                console.log('loadData calls');
+                console.log('all water data', allWaterData);
+
+                if (!(formattedMonth in allWaterData)) {
+                    const firstDate = new Date(Date.UTC(parseInt(currentMonth.year, 10), parseInt(currentMonth.month, 10) - 1, 1));
+                    const lastDate = new Date(Date.UTC(parseInt(currentMonth.year, 10), parseInt(currentMonth.month, 10), 0, 23, 59, 59, 999));
+    
+                    const collectionData = query(
+                        collection(db, "water_tracking"),
+                        where("date", ">=", firstDate),
+                        where("date", "<=", lastDate),
+                        where("user_id", "==", session)
+                    );
+                    const docSnap = await getDocs(collectionData);
+                    const docData: WaterDataEntry[] = [];
+    
+                    // console.log('loadData running.');
+                    // console.log('docSnap', docSnap);
+    
+                    docSnap.forEach((item) => {
+                        const data = item.data();
+                        const entry: WaterDataEntry = {
+                            date: String(data.date.toDate()),
+                            intake_amount: data.intake_amount,
+                            user_id: data.user_id,
+                            waterType: data.waterType
+                        };
+                        docData.push(entry);
+                    });
+    
+                    console.log('docData', docData);
+    
+                    console.log({ [formattedMonth]: docData });
+    
+                    dispatch(pushWaterData({ [formattedMonth]: docData }));
+                }
+            }
+            catch (error: any) {
+                console.log('error in fetchWater', error);
+                console.log('errro.message', error.message);
+            }
+
+        }
+        loadData();
+    }
+
+    console.log('formattedMonth', formattedMonth);
+
+    console.log(allWaterData[formattedMonth]);
+
+    if (!(formattedMonth in allWaterData) || (allWaterData[formattedMonth].length < 1)) { return <></> }
 
     return (
         <Card style={[{ marginHorizontal: 10 }]}>
--- store/trackSlice.tsx
+++ store/trackSlice.tsx
@@ -1,35 +1,51 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+interface WaterDataEntry {
+    date: string;
+    intake_amount: number;
+    user_id: string;
+    waterType: string;
+}
+
+type WaterDataState = {
+    [key: string]: WaterDataEntry[];
+};
+
 interface TrackState {
     currentDate: string;
     loadingTrackDate: boolean;
-    currentMonth: { month: string; year: string; }
+    currentMonth: { month: string; year: string; };
+    waterData: WaterDataState | [];
 }
 
 const initialState: TrackState = {
     currentDate: `${new Date().getFullYear()}-${String(new Date().getMonth() + 1).padStart(2, "0")}-${String(new Date().getDate()).padStart(2, "0")}`,
     loadingTrackDate: true,
-    currentMonth: { month: String(new Date().getMonth() + 1).padStart(2, "0"), year: String(new Date().getFullYear()) }
+    currentMonth: { month: String(new Date().getMonth() + 1).padStart(2, "0"), year: String(new Date().getFullYear()) },
+    waterData: []
 };
 
 const trackSlice = createSlice({
-    name: 'camera',
+    name: 'track',
     initialState,
     reducers: {
-        setCurrentDate: (state, action: PayloadAction<string>) => {
+        setCurrentDate: (state: TrackState, action: PayloadAction<string>) => {
             state.currentDate = action.payload;
         },
-        setShowLoadingTrackDate: (state, action: PayloadAction<string>) => {
+        setShowLoadingTrackDate: (state: TrackState, action: PayloadAction<string>) => {
             state.loadingTrackDate = true;
         },
-        setHideLoadingTrackDate: (state, action: PayloadAction<string>) => {
+        setHideLoadingTrackDate: (state: TrackState, action: PayloadAction<string>) => {
             state.loadingTrackDate = false;
         },
-        setCurrentMonth: (state, action: PayloadAction<{month: string; year: string}>) => {
+        setCurrentMonth: (state: TrackState, action: PayloadAction<{ month: string; year: string }>) => {
             state.currentMonth = action.payload;
-        } 
+        },
+        pushWaterData: (state: TrackState, action: PayloadAction<WaterDataState>) => {
+            state.waterData = { ...state.waterData, ...action.payload };
+        }
     },
 });
 
-export const { setCurrentDate, setShowLoadingTrackDate, setHideLoadingTrackDate, setCurrentMonth } = trackSlice.actions;
+export const { setCurrentDate, setShowLoadingTrackDate, setHideLoadingTrackDate, setCurrentMonth, pushWaterData } = trackSlice.actions;
 export default trackSlice.reducer;
