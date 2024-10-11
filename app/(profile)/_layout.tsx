f79114c401af3b87df0a97c84c2185a068bf5f25
Habib786 <habib-m-farooq@hotmail.com>
2024-10-11 20:22:34 +0300

    Secure layout with session storage.

--- app/(profile)/_layout.tsx
+++ app/(profile)/_layout.tsx
@@ -1,6 +1,14 @@
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
+import { useSession } from '../../ctx';
+import { View } from "react-native";
+import { Text } from "react-native-paper";
 
 export default function Layout() {
+  const { session, isLoading } = useSession();
+
+  if (isLoading) { return <View><Text variant="displayLarge">Loading...</Text></View> }
+  if (!session) { return <Redirect href="/login" />; }
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
--- app/(signup_questionnaire)/_layout.tsx
+++ app/(signup_questionnaire)/_layout.tsx
@@ -1,12 +1,17 @@
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
+import { useSession } from '../../ctx';
+import { View } from "react-native";
+import { Text } from "react-native-paper";
 
 export default function Layout() {
+  const { session, isLoading } = useSession();
+
+  if (isLoading) { return <View><Text variant="displayLarge">Loading...</Text></View> }
+  if (!session) { return <Redirect href="/login" />; }
+  
   return (
-    <>
-      <Stack.Screen options={{ headerShown: false }} />
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
-    </>
-  );
+    );
 }
