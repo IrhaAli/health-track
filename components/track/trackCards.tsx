8f3b8865ee27fc135672c7e04edf4f724632df2e
Habib786 <habib-m-farooq@hotmail.com>
2024-10-12 19:48:09 +0300

    Fetch water data into cards.

--- components/track/trackCards.tsx
+++ components/track/trackCards.tsx
@@ -1,15 +1,73 @@
-import React from "react";
 import { ScrollView } from "react-native";
+import React from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "@/firebaseConfig";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "@/store/store";
+import { pushWaterData } from "@/store/trackSlice";
+import { getAuth } from "firebase/auth";
+import { useSession } from "@/ctx";
 
 // Local Components Start.
 import TrackWaterCard from "./trackWaterCard";
 // Local Components End.
 
+interface WaterDataEntry {
+    id: string;
+    date: string;
+    intake_amount: number;
+    user_id: string;
+    waterType: string;
+}
+
+type WaterDataState = {
+    [key: string]: WaterDataEntry[];
+};
+
 export default function TrackCards() {
+    const currentMonth = useSelector((state: RootState) => state.track.currentMonth);
+    const dispatch = useDispatch();
+    const waterData: WaterDataState | [] = useSelector((state: RootState) => state.track.waterData);
+    const auth = getAuth();
+
+    const { session } = useSession();
+    const user = auth.currentUser;
+    const formattedMonth: string = String(`${currentMonth.year}-${currentMonth.month}`);
+
+    const firstDate = new Date(Date.UTC(parseInt(currentMonth.year, 10), parseInt(currentMonth.month, 10) - 1, 1));
+    const lastDate = new Date(Date.UTC(parseInt(currentMonth.year, 10), parseInt(currentMonth.month, 10), 0, 23, 59, 59, 999));
+
+    const loadWaterData = async () => {
+        try {
+            if (!(formattedMonth in waterData)) {
+                const collectionData = query(
+                    collection(db, "water_tracking"),
+                    where("date", ">=", firstDate),
+                    where("date", "<=", lastDate),
+                    where("user_id", "==", session)
+                );
+                const docSnap = await getDocs(collectionData);
+
+                const docData = docSnap.docs.map(item => ({
+                    id: item.id,
+                    date: item.data().date.toDate().toISOString(),
+                    intake_amount: item.data().intake_amount,
+                    user_id: item.data().user_id,
+                    waterType: item.data().waterType
+                }));
+                dispatch(pushWaterData({ [formattedMonth]: docData }));
+            }
+        }
+        catch (error: any) { }
+    }
+
+    if (user?.uid && currentMonth.month && currentMonth.year) {
+        loadWaterData();
+    }
+
     return (
         <ScrollView>
             <TrackWaterCard></TrackWaterCard>
-            {/* <Divider style={[{ marginVertical: 10 }]} /> */}
         </ScrollView>
     );
 }
--- components/track/trackWaterCard.tsx
+++ components/track/trackWaterCard.tsx
@@ -1,12 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Card, Button, Text, Avatar } from 'react-native-paper';
-import { collection, getDocs, query, where } from "firebase/firestore";
-import { db } from "@/firebaseConfig";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
-import { pushWaterData } from "@/store/trackSlice";
-import { getAuth } from "firebase/auth";
-import { useSession } from "@/ctx";
 
 interface WaterDataEntry {
     date: string;
@@ -20,91 +15,40 @@ type WaterDataState = {
 };
 
 export default function TrackWaterCard() {
-    const userId = useSelector((state: RootState) => state.user.userId);
     const currentMonth = useSelector((state: RootState) => state.track.currentMonth);
     const LeftContent = (props: any) => <Avatar.Icon {...props} icon="glass-pint-outline" />
-    const dispatch = useDispatch();
-    const allWaterData: WaterDataState | [] = useSelector((state: RootState) => state.track.waterData);
-    const [waterData, setWaterData] = useState(null);
-    const auth = getAuth();
+    const waterData: WaterDataState | [] = useSelector((state: RootState) => state.track.waterData);
+    const currentDate: string = useSelector((state: RootState) => state.track.currentDate);
+    const formattedMonth: string = String(`${currentMonth.year}-${currentMonth.month}`);
 
-    const { session } = useSession();
-    const user = auth.currentUser;
-    const formattedMonth: string = String(`${currentMonth.year}-${currentMonth.month}`); 
-
-    console.log('user?.uid', user?.uid);
-
-    if (user?.uid && currentMonth.month && currentMonth.year) {
-        const loadData = async () => {
-            try {
-                console.log('loadData calls');
-                console.log('all water data', allWaterData);
-
-                if (!(formattedMonth in allWaterData)) {
-                    const firstDate = new Date(Date.UTC(parseInt(currentMonth.year, 10), parseInt(currentMonth.month, 10) - 1, 1));
-                    const lastDate = new Date(Date.UTC(parseInt(currentMonth.year, 10), parseInt(currentMonth.month, 10), 0, 23, 59, 59, 999));
-    
-                    const collectionData = query(
-                        collection(db, "water_tracking"),
-                        where("date", ">=", firstDate),
-                        where("date", "<=", lastDate),
-                        where("user_id", "==", session)
-                    );
-                    const docSnap = await getDocs(collectionData);
-                    const docData: WaterDataEntry[] = [];
-    
-                    // console.log('loadData running.');
-                    // console.log('docSnap', docSnap);
-    
-                    docSnap.forEach((item) => {
-                        const data = item.data();
-                        const entry: WaterDataEntry = {
-                            date: String(data.date.toDate()),
-                            intake_amount: data.intake_amount,
-                            user_id: data.user_id,
-                            waterType: data.waterType
-                        };
-                        docData.push(entry);
-                    });
-    
-                    console.log('docData', docData);
-    
-                    console.log({ [formattedMonth]: docData });
-    
-                    dispatch(pushWaterData({ [formattedMonth]: docData }));
+    if (!Array.isArray(waterData)) {
+        if (formattedMonth in waterData) {
+            if (waterData[formattedMonth] && waterData[formattedMonth].length > 0) {
+                {
+                    return waterData[formattedMonth]
+                        .filter(entry => new Date(entry.date).toISOString().split('T')[0] === currentDate)
+                        .map((water, index) => (
+                            <Card key={index} style={{ marginHorizontal: 10, marginVertical: 10 }}>
+                                <Card.Title
+                                    title={`Date: ${new Date(water.date).toLocaleDateString()}`}
+                                    subtitle={`User ID: ${water.user_id}`}
+                                    left={LeftContent}
+                                />
+                                <Card.Content>
+                                    <Text variant="titleLarge">
+                                        Intake Amount: {water.intake_amount} {water.waterType}
+                                    </Text>
+                                </Card.Content>
+                                <Card.Actions>
+                                    <Button icon="delete">Delete</Button>
+                                    <Button icon="pencil">Edit</Button>
+                                </Card.Actions>
+                            </Card>
+                        ))
                 }
             }
-            catch (error: any) {
-                console.log('error in fetchWater', error);
-                console.log('errro.message', error.message);
-            }
-
-        }
-        loadData();
-    }
-
-
-    
-
-    if (!Array.isArray(allWaterData)) { 
-        console.log('Array true');
-        if (!(formattedMonth in allWaterData)) { return <></> }
-        else if (!allWaterData[formattedMonth] || !allWaterData[formattedMonth] || allWaterData[formattedMonth].length < 1) {
-            return <></>
         }
     }
 
-    return (
-        <Card style={[{ marginHorizontal: 10 }]}>
-            <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
-            <Card.Content>
-                <Text variant="titleLarge">Card title</Text>
-                <Text variant="bodyMedium">Card content</Text>
-            </Card.Content>
-            <Card.Actions>
-                <Button icon="delete">Delete</Button>
-                <Button icon="pencil">Edit</Button>
-            </Card.Actions>
-        </Card>
-    );
+    return (<></>);
 }
