4575c77ac146211fa1e2fd849413ca92f7a098b8
Habib786 <habib-m-farooq@hotmail.com>
2024-10-15 19:36:22 +0300

    Added track sleep data interfaces.

--- types/track.ts
+++ types/track.ts
@@ -6,14 +6,28 @@ export interface WaterDataEntry {
   waterType: string;
 }
 
+export interface SleepDataEntry {
+  id?: string;
+  bed_time: string | Date;
+  sleep_duration: number;
+  sleep_quality: number;
+  user_id: string;
+  wakeup_time: string | Date;
+}
+
 export type WaterDataState = {
   [key: string]: WaterDataEntry[];
+};
+
+export type SleepDataState = {
+  [key: string]: SleepDataEntry[];
 };
 
 export interface TrackState {
   currentDate: string;
   currentMonth: { month: string; year: string; };
   waterData: WaterDataState;
+  sleepData: SleepDataState;
   loadingTrackWaterData: boolean;
   loadingTrackDietData: boolean;
   loadingTrackSleepData: boolean;
