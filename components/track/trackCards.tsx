7bc00aea97d6394153d752dd84b0c3e68f4f5d41
Habib786 <habib-m-farooq@hotmail.com>
2024-10-17 21:38:49 +0300

    Added weight card in cards component.

--- components/track/trackCards.tsx
+++ components/track/trackCards.tsx
@@ -2,12 +2,13 @@ import { ScrollView } from "react-native";
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { AppDispatch, RootState } from "@/store/store";
-import { fetchSleepData, fetchWaterData } from "@/store/trackSlice";
+import { fetchSleepData, fetchWaterData, fetchWeightData } from "@/store/trackSlice";
 import { getAuth } from "firebase/auth";
 
 // Local Components Start.
 import TrackWaterCard from "./trackWaterCard";
 import TrackSleepCard from "./trackSleepCard";
+import TrackWeightCard from "./trackWeightCard";
 // Local Components End.
 
 export default function TrackCards() {
@@ -21,6 +22,7 @@ export default function TrackCards() {
         if (user?.uid && currentMonth.month && currentMonth.year) {
             dispatch(fetchWaterData({ month: String(currentMonth.month), year: String(currentMonth.year), userId: String(user.uid) }));
             dispatch(fetchSleepData({ month: String(currentMonth.month), year: String(currentMonth.year), userId: String(user.uid) }));
+            dispatch(fetchWeightData({ month: String(currentMonth.month), year: String(currentMonth.year), userId: String(user.uid) }));
         }
     }, [user, currentMonth, dispatch]);
 
@@ -28,6 +30,7 @@ export default function TrackCards() {
         <ScrollView>
             <TrackWaterCard></TrackWaterCard>
             <TrackSleepCard></TrackSleepCard>
+            <TrackWeightCard></TrackWeightCard>
         </ScrollView>
     );
 }
