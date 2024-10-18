e2f7def7469b7162f87c33e6980fdf2ae332dcde
Habib786 <habib-m-farooq@hotmail.com>
2024-10-19 00:59:13 +0300

    Added feature in dialog to use for edit.

--- components/track/trackDialog.tsx
+++ components/track/trackDialog.tsx
@@ -14,86 +14,43 @@ export default function TrackDialog() {
     const dispatch = useDispatch<AppDispatch>();
     const currentDate = useSelector((state: RootState) => state.track.currentDate);
     const dialogTab = useSelector((state: RootState) => state.trackDialog.dialogTab);
+    const dialogType = useSelector((state: RootState) => state.trackDialog.dialogType);
 
     return (
         <>
-            <Button mode="contained" icon="book-plus" style={[{alignSelf: 'flex-end', position: "absolute", bottom: 5, right: 5}]} uppercase onPress={() => dispatch(setDialog({ showDialog: true, dialogTab: 'sleep', dialogType: 'add' }))}>Add Track</Button>
+            <Button mode="contained" icon="book-plus" style={[{ alignSelf: 'flex-end', position: "absolute", bottom: 5, right: 5 }]} uppercase onPress={() => dispatch(setDialog({ showDialog: true, dialogTab: 'sleep', dialogType: 'ADD' }))}>Add Track</Button>
 
             <Portal>
                 <Dialog visible={dialogStatus} dismissable={false} onDismiss={() => dispatch(setDialog({ showDialog: false, dialogTab: null, dialogType: null }))}>
-                    <Dialog.Title>{`Add ${new Date(currentDate).toLocaleString("default", { month: "short", })}, ${new Date(currentDate).getDate()} 's `}<Text style={[{textTransform: 'capitalize'}]}>{dialogTab}</Text>{` Data`}</Dialog.Title>
+                    <Dialog.Title>{`Add ${new Date(currentDate).toLocaleString("default", { month: "short", })}, ${new Date(currentDate).getDate()} 's `}<Text style={[{ textTransform: 'capitalize' }]}>{dialogTab}</Text>{` Data`}</Dialog.Title>
                     <Dialog.Content>
-                        <SegmentedButtons
-                            value={dialogTab ? dialogTab : 'sleep'}
-                            onValueChange={(value: string) => { dispatch(setTab(value)); dispatch(clearImageURI()); }}
-                            buttons={[
-                                { value: 'sleep', label: 'Sleep', icon: 'moon-waning-crescent' },
-                                { value: 'diet', label: 'Diet', icon: 'food' },
-                                { value: 'water', label: 'Water', icon: 'glass-pint-outline' },
-                                { value: 'weight', label: 'Weight', icon: 'weight' },
-                            ]}
-                        />
-                        <Divider style={[{marginTop: 10}]}/>
+                        {dialogType !== 'EDIT' && <>
+                            <SegmentedButtons
+                                value={dialogTab ? dialogTab : 'sleep'}
+                                onValueChange={(value: string) => { dispatch(setTab(value)); dispatch(clearImageURI()); }}
+                                buttons={[
+                                    { value: 'sleep', label: 'Sleep', icon: 'moon-waning-crescent' },
+                                    { value: 'diet', label: 'Diet', icon: 'food' },
+                                    { value: 'water', label: 'Water', icon: 'glass-pint-outline' },
+                                    { value: 'weight', label: 'Weight', icon: 'weight' },
+                                ]}
+                            />
+                            <Divider style={[{ marginTop: 10 }]} />
+                        </>}
                         <TrackForms></TrackForms>
                     </Dialog.Content>
                 </Dialog>
             </Portal>
-            
+
             <AppCamera></AppCamera>
         </>
     );
 }
 
 const styles = StyleSheet.create({
-    trackDialog: {
-        backgroundColor: 'white',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    formTabs: {
-        display: "flex",
-        flexDirection: "row",
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%'
-    },
-    formTabsBody: {
-        backgroundColor: "#808080",
-    },
-    buttonText: {
-        color: "white",
-        fontSize: 18,
-        fontWeight: "bold",
-    },
-    button: {
-        backgroundColor: "red",
-        height: 45,
-        borderColor: "gray",
-        borderWidth: 1,
-        borderRadius: 5,
-        alignItems: "center",
-        justifyContent: "center",
-    },
     tabButton: {
         flex: 1,
         justifyContent: 'center',
         alignContent: 'center'
     },
-    tabButtonText: {
-        color: 'white',
-        backgroundColor: 'red',
-        paddingVertical: 10,
-        paddingHorizontal: 10,
-        fontWeight: '700',
-        textTransform: 'uppercase',
-        borderRadius: 0,
-        fontSize: 15,
-        textAlign: 'center'
-    },
-    dialogTitle: {
-        color: 'black',
-        textTransform: 'capitalize',
-        fontWeight: '700'
-    }
 })
