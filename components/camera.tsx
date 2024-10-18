e6604888ae5aa1c8d112832792ca6620e17ad87e
Habib786 <habib-m-farooq@hotmail.com>
2024-10-19 00:59:54 +0300

    Fix camera glitch on closing.

--- components/camera.tsx
+++ components/camera.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { View, Text, StyleSheet, Modal } from "react-native";
 import { useCameraPermissions } from "expo-camera";
 import { Camera, CameraType } from "expo-camera/legacy";
@@ -7,16 +7,17 @@ import { AppDispatch, RootState } from "@/store/store";
 import { setHideCamera, setImageURI } from "@/store/cameraSlice";
 import { Portal, Button, Avatar } from "react-native-paper";
 import { useWindowDimensions } from "react-native";
+import { useIsFocused } from '@react-navigation/native';
 
 export default function AppCamera() {
     const showCamera = useSelector((state: RootState) => state.camera.showCamera)
     const [permission, requestPermission] = useCameraPermissions();
     const cameraRef = useRef<Camera | null>(null);
-    const [timer, setTimer] = useState("");
     const [cameraSide, setCameraSide] = useState(CameraType.back);
     const dispatch = useDispatch<AppDispatch>();
     const { width } = useWindowDimensions();
     const height = Math.round((width * 16) / 9);
+    const isFocused = useIsFocused();
 
     const toggleCameraFacing = () => {
         setCameraSide((current) =>
@@ -25,50 +26,77 @@ export default function AppCamera() {
     };
 
     const takePhoto = async () => {
-        const data: any = await cameraRef?.current?.takePictureAsync(undefined);
-        dispatch(setImageURI(data.uri));
-        setTimer("");
-        dispatch(setHideCamera());
+        if (cameraRef.current) {
+            const data = await cameraRef.current.takePictureAsync();
+            dispatch(setImageURI(data.uri));
+            dispatch(setHideCamera());
+        }
     };
 
-    return (
-        <Portal>
-            <Modal presentationStyle="overFullScreen" visible={showCamera}>
-                {showCamera && (!permission ? (
-                    <View />
-                ) : !permission.granted ? (
-                    <View>
-                        <View>
-                            <Text style={styles.message}>We need your permission to show the camera</Text>
-                            <Button onPress={requestPermission} >grant permission</Button>
-                        </View>
-                    </View>
-                ) : (
-                    <View style={[{ height: '100%', width: '100%', backgroundColor: '#000', justifyContent: 'center' }]}>
-                        <Camera style={[{ width: '100%', height, alignSelf: 'center', zIndex: 999, backgroundColor: '#000', justifyContent: 'flex-end' }]} type={cameraSide} ratio="16:9" ref={cameraRef} focusable={true} autoFocus={true}>
+    const handleCloseCamera = async () => {
+        if (cameraRef.current) {
+            await cameraRef.current.pausePreview();
+            cameraRef.current = null;
+        }
+        setTimeout(() => {
+            dispatch(setHideCamera());
+        }, 300); // Delay to ensure smooth transition
+    };
+
+    if (!permission) {
+        return <View />;
+    } else if (!permission.granted) {
+        return (
+            <View>
+                <Text style={styles.message}>
+                    We need your permission to show the camera
+                </Text>
+                <Button onPress={requestPermission}>Grant Permission</Button>
+            </View>
+        );
+    } else if (isFocused && showCamera) {
+        return (
+            <Portal>
+                <Modal presentationStyle="overFullScreen" visible={showCamera}>
+                    <View style={styles.cameraContainer}>
+                        <Camera style={styles.camera} type={cameraSide} ratio="16:9" ref={cameraRef} >
                             <View style={styles.buttonContainer}>
                                 <Button mode="text" onPress={toggleCameraFacing} theme={{ colors: { primary: 'white' } }} icon={({ size, color }) => (<Avatar.Icon size={48} icon="camera-flip" color="#fff" />)}>{''}</Button>
-
                                 <Button mode="text" onPress={takePhoto} theme={{ colors: { primary: 'white' } }} icon={({ size, color }) => (<Avatar.Icon size={48} icon="camera" color="#fff" />)}>{''}</Button>
-
-                                <Button mode="text" onPress={() => { dispatch(setHideCamera()); }} theme={{ colors: { primary: 'white' } }} icon={({ size, color }) => (<Avatar.Icon size={48} icon="close" color="#fff" />)}>{''}</Button>
+                                <Button mode="text" onPress={handleCloseCamera} theme={{ colors: { primary: 'white' } }} icon={({ size, color }) => (<Avatar.Icon size={48} icon="close" color="#fff" />)}>{''}</Button>
                             </View>
                         </Camera>
                     </View>
-                ))}
-            </Modal>
-        </Portal>
-    )
+                </Modal>
+            </Portal>
+        );
+    } else {
+        return <View />;
+    }
+
 }
 
 const styles = StyleSheet.create({
+    cameraContainer: {
+        height: '100%',
+        width: '100%',
+        backgroundColor: '#000',
+        justifyContent: 'center',
+    },
+    camera: {
+        width: '100%',
+        height: '100%',
+        alignSelf: 'center',
+        zIndex: 999,
+        backgroundColor: '#000',
+    },
     buttonContainer: {
+        position: 'absolute',
+        bottom: 0,
         width: '100%',
         flexDirection: "row",
-        alignItems: 'flex-end',
         justifyContent: 'space-between',
-        alignSelf: 'flex-start',
-        paddingBottom: 10
+        paddingBottom: 10,
     },
     message: {
         textAlign: "center",
