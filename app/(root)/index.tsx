22852792e9cd85faa168d50d8eb80df5b7142ea3
Habib <habib-m-farooq@hotmail.com>
2024-11-07 00:59:04 +0300

    Remove home old code and converted it into component.

--- app/(root)/index.tsx
+++ app/(root)/index.tsx
@@ -2,54 +2,12 @@ import { Image, StyleSheet, Platform } from "react-native";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from '@/components/ThemedView';
+import HomeComponent from "@/components/home/home";
+import React from "react";
 
 export default function HomeScreen() {
   return (
-    <>
-      <ParallaxScrollView
-        headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-        headerImage={
-          <Image
-            source={require("@/assets/images/home-image.png")}
-            style={styles.appLogo}
-          />
-        }
-      >
-        <ThemedView style={styles.titleContainer}>
-          <ThemedText type="title">My Health Tracker</ThemedText>
-        </ThemedView>
-        <ThemedView style={styles.stepContainer}>
-          <ThemedText type="subtitle">Average Fasting Time</ThemedText>
-          <ThemedText>
-            This is the average fasting period since start of recording till now.
-          </ThemedText>
-        </ThemedView>
-        <ThemedView style={styles.stepContainer}>
-          <ThemedText type="subtitle">Average Number of Meals</ThemedText>
-          <ThemedText>
-            This is the average number of meals/day (including snacks) since start of recording till now.
-          </ThemedText>
-        </ThemedView>
-        <ThemedView style={styles.stepContainer}>
-          <ThemedText type="subtitle">Average Sleeping Hours</ThemedText>
-          <ThemedText>
-            This is the average number of sleeping hours/night since start of recording till now.
-          </ThemedText>
-        </ThemedView>
-        <ThemedView style={styles.stepContainer}>
-          <ThemedText type="subtitle">Average Water Consumption (Cups)</ThemedText>
-          <ThemedText>
-            This is the average number of water drank/day since start of recording till now.
-          </ThemedText>
-        </ThemedView>
-        <ThemedView style={styles.stepContainer}>
-          <ThemedText type="subtitle">Average Bi-Weekly Weight Change</ThemedText>
-          <ThemedText>
-            This is the average weight/bi-weekly since start of recording till now.
-          </ThemedText>
-        </ThemedView>
-      </ParallaxScrollView>
-    </>
+    <HomeComponent />
   );
 }
 
