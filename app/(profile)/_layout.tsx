97ac3883dc5dee18e54cbd6db2e277414263f326
Habib <habib-m-farooq@hotmail.com>
2024-11-09 00:49:13 +0300

    Remove session check and remove loading.

--- app/(profile)/_layout.tsx
+++ app/(profile)/_layout.tsx
@@ -1,22 +1,7 @@
-import { Redirect, Stack } from "expo-router";
-import { useSession } from '../../ctx';
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { Stack } from "expo-router";
 import React from "react";
 
 export default function Layout() {
-  const { session, isLoading, isFirstLaunch } = useSession();
-
-  if (isLoading) { return <View><Text variant="displayLarge">Loading...</Text></View> }
-  
-  if (isFirstLaunch) {
-    return <Redirect href="/language" />;
-  }
-
-  if (!session) {
-    return <Redirect href="/login" />;
-  }
-
   return (
     <Stack>
       <Stack.Screen name="background_information" options={{ headerShown: false }} />
--- app/(signup_questionnaire)/_layout.tsx
+++ app/(signup_questionnaire)/_layout.tsx
@@ -1,25 +1,10 @@
-import { Redirect, Stack } from "expo-router";
-import { useSession } from '../../ctx';
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { Stack } from "expo-router";
 import React from "react";
 
 export default function Layout() {
-  const { session, isLoading, isFirstLaunch } = useSession();
-
-  if (isLoading) { return <View><Text variant="displayLarge">Loading...</Text></View> }
-  
-  if (isFirstLaunch) {
-    return <Redirect href="/language" />;
-  }
-
-  if (!session) { 
-    return <Redirect href="/login" />; 
-  }
-  
   return (
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-      </Stack>
-    );
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
