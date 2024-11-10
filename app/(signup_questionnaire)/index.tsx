00bce1f354574158ef674dcc59c8a1cce471c629
Habib <habib-m-farooq@hotmail.com>
2024-11-10 16:06:52 +0300

    Removed unused css and code formatting.

--- app/(signup_questionnaire)/index.tsx
+++ app/(signup_questionnaire)/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Alert, Image, Pressable, StyleSheet, View, Text } from "react-native";
+import { Alert, StyleSheet, View } from "react-native";
 import { useLocalSearchParams, router } from "expo-router";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../../firebaseConfig";
 import UserDetails from "@/components/user_info/UserDetails";
 import { ScrollView } from "react-native";
+import { Button, Surface } from "react-native-paper";
 
 interface UserDetails {
   fullName: string;
@@ -71,154 +72,38 @@ export default function UserDetailsPanel() {
 
   return (
     <ScrollView>
-      <UserDetails
-        userDetails={UserDetails}
-        setUserDetails={setUserDetails}
-        isSignUpPage={true}
-      />
-      <View style={styles.buttonView}>
-        <Pressable style={styles.button} onPress={onSubmit}>
-          <Text style={styles.buttonText}>NEXT</Text>
-        </Pressable>
-      </View>
+      <Surface style={styles.container}>
+        <UserDetails
+          userDetails={UserDetails}
+          setUserDetails={setUserDetails}
+          isSignUpPage={true}
+        />
+        <View style={styles.buttonContainer}>
+          <Button 
+            mode="contained" 
+            onPress={onSubmit}
+            style={styles.button}
+          >
+            Next
+          </Button>
+        </View>
+      </Surface>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  appLogo: {
-    height: 250,
-    width: 400,
-  },
-  socialIcons: {
-    display: "flex",
-    flexDirection: "row",
-  },
   container: {
-    alignItems: "center",
-    paddingTop: 70,
+    flex: 1,
+    padding: 16,
+    elevation: 1,
+    borderRadius: 12
   },
-  image: {
-    height: 160,
-    width: 170,
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: "bold",
-    textTransform: "uppercase",
-    textAlign: "center",
-    paddingVertical: 40,
-    color: "red",
-  },
-  inputView: {
-    gap: 15,
-    width: "100%",
-    paddingHorizontal: 40,
-    marginBottom: 5,
-  },
-  input: {
-    height: 50,
-    paddingHorizontal: 20,
-    borderColor: "red",
-    borderWidth: 1,
-    borderRadius: 7,
-  },
-  rememberView: {
-    width: "100%",
-    paddingHorizontal: 50,
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexDirection: "row",
-    marginBottom: 8,
-  },
-  switch: {
-    flexDirection: "row",
-    gap: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  rememberText: {
-    fontSize: 13,
-  },
-  forgetText: {
-    fontSize: 11,
-    color: "red",
+  buttonContainer: {
+    paddingHorizontal: 16,
+    paddingVertical: 24
   },
   button: {
-    backgroundColor: "red",
-    height: 45,
-    borderColor: "gray",
-    borderWidth: 1,
-    borderRadius: 5,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  buttonView: {
-    width: "100%",
-    paddingHorizontal: 50,
-  },
-  optionsText: {
-    textAlign: "center",
-    paddingVertical: 10,
-    color: "gray",
-    fontSize: 13,
-    marginBottom: 6,
-  },
-  mediaIcons: {
-    flexDirection: "row",
-    gap: 15,
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 23,
-  },
-  icons: {
-    width: 40,
-    height: 40,
-  },
-  footerText: {
-    textAlign: "center",
-    color: "gray",
-  },
-  signup: {
-    color: "red",
-    fontSize: 13,
-  },
-  dropdown: {
-    height: 50,
-    borderColor: "gray",
-    borderWidth: 0.5,
-    borderRadius: 8,
-    paddingHorizontal: 8,
-  },
-  icon: {
-    marginRight: 5,
-  },
-  label: {
-    position: "absolute",
-    backgroundColor: "white",
-    left: 22,
-    top: 8,
-    zIndex: 999,
-    paddingHorizontal: 8,
-    fontSize: 14,
-  },
-  placeholderStyle: {
-    fontSize: 16,
-  },
-  selectedTextStyle: {
-    fontSize: 16,
-  },
-  iconStyle: {
-    width: 20,
-    height: 20,
-  },
-  inputSearchStyle: {
-    height: 40,
-    fontSize: 16,
-  },
+    borderRadius: 8
+  }
 });
