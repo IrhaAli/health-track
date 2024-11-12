649559d45802468c8a65c1fe44d8b3e7264b6c8d
Habib <habib-m-farooq@hotmail.com>
2024-11-12 15:55:55 +0300

    Move firebase config file to services folder.

---
+++ services/firebaseConfig.js
@@ -0,0 +1,33 @@
+import { initializeApp, getApps } from 'firebase/app';
+import { getFirestore } from "firebase/firestore";
+import { initializeAuth, getAuth } from "firebase/auth"
+import { getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getStorage } from "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  databaseURL: 'https://health-app-97.firebase.io/(default)',
+  apiKey: "AIzaSyBwpovW3dwMlah_eGc2PS5dgBOaQsRh5Xc",
+  authDomain: "health-app-97.firebaseapp.com",
+  projectId: "health-app-97",
+  storageBucket: "health-app-97.appspot.com",
+  messagingSenderId: "321117441221",
+  appId: "1:321117441221:web:93d44d5a86532e86f8f4ac",
+  measurementId: "G-MZ5GTWZVYT"
+};
+
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApps()[0];
+}
+
+const storage = getStorage(app);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+const db = getFirestore(app);
+
+export { db, auth, storage };
