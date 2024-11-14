f2ba401a7e4df892099dc6ff6dd856fb70a23ebc
Habib <habib-m-farooq@hotmail.com>
2024-11-14 17:21:58 +0300

    Remove package expo localization.

--- services/i18n.ts
+++ services/i18n.ts
@@ -1,5 +1,4 @@
 import { I18n } from 'i18n-js';
-import * as Localization from 'expo-localization';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import en from '@/translations/en.json';
@@ -17,7 +16,7 @@ i18n.enableFallback = true;
 i18n.defaultLocale = 'en';
 
 // Initialize with default locale
-i18n.locale = Localization.locale || 'en';
+i18n.locale = 'en';
 
 // Function to set/update locale
 export const setI18nConfig = async () => {
