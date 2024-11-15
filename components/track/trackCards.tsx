4fa0f212ac0e501e95002e967bab969b7edfb296
Habib <habib-m-farooq@hotmail.com>
2024-11-15 17:14:10 +0300

    Refactor track cards code.

--- components/track/trackCards.tsx
+++ components/track/trackCards.tsx
@@ -1,43 +1,42 @@
 import { ScrollView } from "react-native";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { AppDispatch, RootState } from "@/store/store";
 import { fetchDietData, fetchSleepData, fetchWaterData, fetchWeightData } from "@/store/trackSlice";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// Local Components Start.
 import TrackWaterCard from "./trackWaterCard";
 import TrackSleepCard from "./trackSleepCard";
 import TrackWeightCard from "./trackWeightCard";
 import TrackDietCard from "./trackDietCard";
-// Local Components End.
 
 export default function TrackCards() {
-    const currentMonth = useSelector((state: RootState) => state.track.currentMonth);
-    const currentDate = useSelector((state: RootState) => state.track.currentDate);
+    const { currentMonth, currentDate } = useSelector((state: RootState) => state.track);
     const dispatch = useDispatch<AppDispatch>();
-    const [currentUser, setCurrentUser] = useState<any>(null);
     const scrollViewRef = useRef<ScrollView>(null);
 
     useEffect(() => {
-        const getUser = async () => {
+        (async () => {
             const userString = await AsyncStorage.getItem('session');
-            if (userString) {
-                const user = JSON.parse(userString);
-                setCurrentUser(user);
-            }
-        };
-        getUser();
-    }, []);
+            if (!userString) return;
 
-    useEffect(() => {
-        if (currentUser?.uid && currentMonth.month && currentMonth.year) {
-            dispatch(fetchWaterData({ month: String(currentMonth.month), year: String(currentMonth.year), userId: String(currentUser.uid) }));
-            dispatch(fetchSleepData({ month: String(currentMonth.month), year: String(currentMonth.year), userId: String(currentUser.uid) }));
-            dispatch(fetchWeightData({ month: String(currentMonth.month), year: String(currentMonth.year), userId: String(currentUser.uid) }));
-            dispatch(fetchDietData({ month: String(currentMonth.month), year: String(currentMonth.year), userId: String(currentUser.uid) }));
-        }
-    }, [currentUser, currentMonth, dispatch]);
+            const { uid } = JSON.parse(userString);
+            const { month, year } = currentMonth;
+
+            const params = {
+                month: String(month),
+                year: String(year), 
+                userId: String(uid)
+            };
+
+            await Promise.all([
+                dispatch(fetchWaterData(params)),
+                dispatch(fetchSleepData(params)), 
+                dispatch(fetchWeightData(params)),
+                dispatch(fetchDietData(params))
+            ]);
+        })();
+    }, [currentMonth, dispatch]);
 
     useEffect(() => {
         scrollViewRef.current?.scrollTo({ y: 0, animated: true });
@@ -45,10 +44,10 @@ export default function TrackCards() {
 
     return (
         <ScrollView ref={scrollViewRef}>
-            <TrackWaterCard></TrackWaterCard>
-            <TrackSleepCard></TrackSleepCard>
-            <TrackWeightCard></TrackWeightCard>
-            <TrackDietCard></TrackDietCard>
+            <TrackWaterCard />
+            <TrackSleepCard />
+            <TrackWeightCard />
+            <TrackDietCard />
         </ScrollView>
     );
 }
