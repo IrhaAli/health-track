1b38eb168d6774a13095ab4eed36dab0c99c9485
Habib <habib-m-farooq@hotmail.com>
2024-11-15 17:14:47 +0300

    Refactor track forms code.

--- components/track/trackForms.tsx
+++ components/track/trackForms.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { View } from "react-native";
-import TrackSleepForm from "./trackSleepForm";
-import TrackWaterForm from "./trackWaterForm";
-import TrackDietForm from "./trackDietForm";
-import TrackWeightForm from "./trackWeightForm";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
+import TrackSleepForm from "./trackSleepForm";
+import TrackWaterForm from "./trackWaterForm";
+import TrackDietForm from "./trackDietForm"; 
+import TrackWeightForm from "./trackWeightForm";
+import { DialogTab } from "@/store/trackDialogSlice";
+
+const FORM_COMPONENTS: Record<DialogTab, React.ComponentType> = {
+    [DialogTab.SLEEP]: TrackSleepForm,
+    [DialogTab.DIET]: TrackDietForm,
+    [DialogTab.WATER]: TrackWaterForm,
+    [DialogTab.WEIGHT]: TrackWeightForm
+};
 
 export default function TrackForms() {
     const dialogTab = useSelector((state: RootState) => state.trackDialog.dialogTab);
-
+    const FormComponent = dialogTab ? FORM_COMPONENTS[dialogTab as DialogTab] : null;
+    
     return (
         <View>
-            {dialogTab === "sleep" && ( <TrackSleepForm></TrackSleepForm> )}
-            {dialogTab === "diet" && ( <TrackDietForm></TrackDietForm> )}
-            {dialogTab === "water" && ( <TrackWaterForm></TrackWaterForm> )}
-            {dialogTab === "weight" && ( <TrackWeightForm></TrackWeightForm> )}
+            {FormComponent && <FormComponent />}
         </View>
-    )
+    );
 }
