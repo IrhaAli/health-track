e91af4506b2dfed63fb81cf0b7d68690bae69f7d
Habib <habib-m-farooq@hotmail.com>
2024-11-15 17:13:41 +0300

    Refactor track page code.

--- components/track/track.tsx
+++ components/track/track.tsx
@@ -1,30 +1,23 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { memo, useEffect, useState } from "react";
 import { Divider } from 'react-native-paper';
 import { InteractionManager } from 'react-native';
-
-// Local Components Start.
 import TrackDialog from "./trackDialog";
 import TrackCalendar from "./trackCalendar";
 import TrackCards from "./trackCards";
-// Local Components End.
+
+const MemoizedTrackCards = memo(TrackCards);
 
 export default function TrackComponent() {
   const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
-    InteractionManager.runAfterInteractions(() => {
-      setIsReady(true);
-    });
-  }, []);
-
-  const MemoizedTrackCards = useCallback(() => {
-    return <TrackCards />;
+    InteractionManager.runAfterInteractions(() => setIsReady(true));
   }, []);
 
   return (
     <>
       <TrackCalendar />
-      <Divider style={[{ marginTop: 10}]} />
+      <Divider style={{ marginTop: 10 }} />
       {isReady && <MemoizedTrackCards />}
       <TrackDialog />
     </>
