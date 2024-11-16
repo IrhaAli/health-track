ed43480089b338e6f3257dc1a59b2eb6540c2078
Habib <habib-m-farooq@hotmail.com>
2024-11-16 23:23:57 +0300

    Added custom navigation.

--- app/(profile)/medical_history.tsx
+++ app/(profile)/medical_history.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import ProfileMedicalHistory from "@/components/profile/ProfileMedicalHistory";
 
 export default function MedicalHistory() {
-  return <ProfileMedicalHistory></ProfileMedicalHistory>;
+  return <ProfileMedicalHistory showNavigation={true}></ProfileMedicalHistory>;
 }
