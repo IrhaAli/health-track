fba9cdb3c70408dd77b4deda561bd9695c74dbcc
Habib <habib-m-farooq@hotmail.com>
2024-11-16 23:23:33 +0300

    Remove header from profile component.

--- app/(profile)/_layout.tsx
+++ app/(profile)/_layout.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="background_information" options={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="background_information" />
     </Stack>
   );
 }
