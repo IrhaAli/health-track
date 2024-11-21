c20c20ae7311c647b09e6e958f708a563a28f7eb
Habib <habib-m-farooq@hotmail.com>
2024-11-22 01:25:50 +0300

    Replace color path.

--- hooks/useThemeColor.ts
+++ hooks/useThemeColor.ts
@@ -5,7 +5,7 @@
 
 import { useColorScheme } from 'react-native';
 
-import { Colors } from '@/constants/Colors';
+import { Colors } from '@/app/theme';
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
